93e135a02bbdeca9eda4666b515601dc88606659
ronakjangir47 <ronakjangir47@gmail.com>
2013-04-28 12:09:05 +0530

    app request popup and show it

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -1,9 +1,9 @@
 class FacebookController < ApplicationController
 
   def canvas
+    @user=User.last
     if params[:code]
       @user = User.fetch_details(params[:code])
-     # @user=User.last
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
   has_many :invities
 
   def self.fetch_details(code)
-    #access_token = $oauth.get_access_token(code)
-    user_details = Koala::Facebook::API.new("BAADR6JvVBzoBAN6zshDlpqGlGOzM2ZBnNg5fZCDhFkYHUiOrtZAxFR77hHaXuTFZAHjovWunzfCpXZAUavbVunblb4RPU8VZAWh8UX1R4ekwoNF4QRZAPO6D6jCJA5F11EZD").get_object("me")
+    access_token = $oauth.get_access_token(code)
+    user_details = Koala::Facebook::API.new(access_token).get_object("me")
     user = find_or_initialize_by_email(user_details["email"]).tap do |u|
       u.first_name = user_details["first_name"]
       u.last_name = user_details["last_name"]
